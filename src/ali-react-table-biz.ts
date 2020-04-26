70460d0dab6e39c2dc14bba5da6e8d4323e8f6b8
feichao.sfc <feichao.sfc@alibaba-inc.com>
2020-04-26 13:49:07 +0800

    chore: 避免 export * as ns 语法

--- src/ali-react-table-biz.ts
+++ src/ali-react-table-biz.ts
@@ -1,4 +1,7 @@
-export * as commonTransforms from './biz/common-transforms'
+import * as commonTransforms from './biz/common-transforms'
+
+export { commonTransforms }
+
 export { default as CustomColumnsDialog, CustomColumnsDialogProps } from './biz/CustomColumnsDialog'
 export { SortItem, TableTransform, SortOrder, Transform } from './biz/interfaces'
 export { TableOperationFeature, useOperationBar } from './biz/operation-bar'

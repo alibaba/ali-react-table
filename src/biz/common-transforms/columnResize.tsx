d0a5faa2fb13fbcfd24c5f1dfc412affafbf87e0
feichao.sfc <feichao.sfc@alibaba-inc.com>
2020-05-11 20:20:42 +0800

    docs: 添加 commonTransforms.resize 文档

--- src/biz/common-transforms/columnResize.tsx
+++ src/biz/common-transforms/columnResize.tsx
@@ -19,12 +19,24 @@ const ResizeHandle = styled.span`
   width: 10px;
   cursor: col-resize;
   z-index: 1;
-
-  transition: background-color 200ms;
-  :hover {
-    background: #00000022;
-  }
 `
+
+export interface ColumnResizeOptions {
+  /** 每一列的宽度 */
+  sizes: number[]
+
+  /** 修改宽度的回调函数 */
+  onChangeSizes(nextSizes: number[]): void
+
+  /** 列的最小宽度，默认为 40 */
+  minSize?: number
+  /** 列的最大宽度，默认为 Infinity */
+  maxSize?: number
+  /** 是否在列的末尾追加可伸缩列，默认为 false */
+  appendExpander?: boolean
+  /** 是否在调整列宽时禁用 user-select，默认为 false */
+  disableUserSelectWhenResizing?: boolean
+}
 
 export default function columnResize({
   sizes,
@@ -33,14 +45,7 @@ export default function columnResize({
   maxSize = Infinity,
   appendExpander,
   disableUserSelectWhenResizing,
-}: {
-  sizes: number[]
-  onChangeSizes(nextSizes: number[]): void
-  minSize?: number
-  maxSize?: number
-  appendExpander?: boolean
-  disableUserSelectWhenResizing?: boolean
-}): TableTransform {
+}: ColumnResizeOptions): TableTransform {
   const startResize = (colIndex: number, e: React.MouseEvent<HTMLSpanElement>) => {
     const startX = e.clientX
     const startSize = sizes[colIndex]
